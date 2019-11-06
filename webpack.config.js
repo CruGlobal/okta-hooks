bdfb30a6484f3526a6485433f1373b365f4313ce
Brian Zoetewey <brian.zoetewey@cru.org>
2019-11-06 13:46:06 -0500

    webpack config to copy serverless.yml include files to the package

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const webpack = require('webpack')
 const slsw = require('serverless-webpack')
 const nodeExternals = require('webpack-node-externals')
 const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const sourcemapVersion = require('child_process').execSync('git rev-parse --short HEAD').toString().trim()
+const { find, get } = require('lodash')
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -17,6 +19,17 @@ module.exports = {
     new webpack.EnvironmentPlugin({
       SOURCEMAP_VERSION: sourcemapVersion
     }),
+    {
+      // Copies files mentioned in serverless.yml package.include to serverless package
+      apply: compiler => {
+        const handler = `${Object.keys(compiler.options.entry)[0]}.handler`
+        const config = find(slsw.lib.serverless.service.functions, val => val.handler === handler)
+        const includePaths = get(config, 'package.include', [])
+        if (includePaths.length) {
+          new CopyWebpackPlugin(includePaths).apply(compiler)
+        }
+      }
+    },
     new RollbarSourceMapPlugin({
       accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
       publicPath: '/var/task',
