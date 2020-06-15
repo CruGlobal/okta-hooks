418198fc46fc0b09cba958c23c87f2d62f12f197
Brian Zoetewey <brian.zoetewey@cru.org>
2020-06-15 11:26:30 -0400

    Sync Okta users missing a GlobalRegistry ID (#6)
    * Update nodejs to 12.x
    * Upgrade packages
    * Add lambda function to sync okta users missing a GR id.

--- __mocks__/@okta/okta-sdk-nodejs.js
+++ __mocks__/@okta/okta-sdk-nodejs.js
@@ -1,4 +1,16 @@
+import forEach from 'lodash/forEach'
+
+const values = {
+  users: []
+}
+export const setUsers = (users = []) => {
+  values.users = users
+}
 export const mockGetUser = jest.fn()
+export const mockListGroupUsers = jest.fn(() => ({
+  each: fn => forEach(values.users, fn)
+}))
 export const Client = jest.fn().mockImplementation(() => ({
-  getUser: mockGetUser
+  getUser: mockGetUser,
+  listGroupUsers: mockListGroupUsers
 }))
