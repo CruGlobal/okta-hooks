bd801005f8814e7dc4ae6c373fcce2f03b7eaa56
Brian Zoetewey <brian.zoetewey@cru.org>
2025-10-21 15:09:36 -0400

    Fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -10,63 +10,63 @@ function git (command) {
 const version = git('rev-parse --short HEAD')
 
 module.exports = {
-    mode: 'production',
-    target: 'node',
-    entry: {
-        registration: './hooks/inline/registration.js',
-        verification: './hooks/event/verification.js',
-        events: './hooks/event/events.js',
-        create: './sns/user/lifecycle/create.js',
-        status_change: './sns/user/lifecycle/status-change.js',
-        update_profile: './sns/user/account/update-profile.js',
-        sync_restricted_domains: './schedule/sync-restricted-domains.js',
-        sync_missing_okta_users: './schedule/sync-missing-okta-users.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        chunkFormat: false,
-        library: {
-            type: 'commonjs2'
+  mode: 'production',
+  target: 'node',
+  entry: {
+    registration: './hooks/inline/registration.js',
+    verification: './hooks/event/verification.js',
+    events: './hooks/event/events.js',
+    create: './sns/user/lifecycle/create.js',
+    status_change: './sns/user/lifecycle/status-change.js',
+    update_profile: './sns/user/account/update-profile.js',
+    sync_restricted_domains: './schedule/sync-restricted-domains.js',
+    sync_missing_okta_users: './schedule/sync-missing-okta-users.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    chunkFormat: false,
+    library: {
+      type: 'commonjs2'
+    }
+  },
+  module: {
+    rules: [{
+      exclude: [
+        /datadog-lambda-js/,
+        /dd-trace/
+      ]
+    }]
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false
+          }
         }
-    },
-    module: {
-        rules: [{
-            exclude: [
-                /datadog-lambda-js/,
-                /dd-trace/
-            ]
-        }]
-    },
-    devtool: 'source-map',
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-                terserOptions: {
-                    format: {
-                        comments: false
-                    }
-                }
-            })
-        ]
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin({
-            SOURCEMAP_VERSION: version
-        }),
-        process.env.CI
-            ? new RollbarSourceMapPlugin({
-                accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-                ignoreErrors: true,
-                publicPath: '/var/task',
-                version
-            })
-            : false
-    ].filter(Boolean),
-    ignoreWarnings: [
-        {
-            message: /aws-crt/
-        }
+      })
     ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      SOURCEMAP_VERSION: version
+    }),
+    process.env.CI
+      ? new RollbarSourceMapPlugin({
+        accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+        ignoreErrors: true,
+        publicPath: '/var/task',
+        version
+      })
+      : false
+  ].filter(Boolean),
+  ignoreWarnings: [
+    {
+      message: /aws-crt/
+    }
+  ]
 }
