011dad54f2ad50f4901ea2b5ff9338b36b502059
Jon Watson <jon.watson@cru.org>
2026-06-09 14:17:51 -0600

    Add orca type, GR put/fields types, and put/listUsers mocks

--- tests/mocks/global-registry-nodejs-client.ts
+++ tests/mocks/global-registry-nodejs-client.ts
@@ -3,11 +3,13 @@ import { vi } from 'vitest'
 export const mockEntityGET = vi.fn()
 export const mockEntityDELETE = vi.fn()
 export const mockEntityPOST = vi.fn()
+export const mockEntityPUT = vi.fn()
 
 export const GRClient = vi.fn().mockImplementation(() => ({
   Entity: {
     get: mockEntityGET,
     delete: mockEntityDELETE,
-    post: mockEntityPOST
+    post: mockEntityPOST,
+    put: mockEntityPUT
   }
 }))
